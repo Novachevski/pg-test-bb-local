bc923d53aa344acccccf194e76821b2fe8c10f6e
Borko Novachevski <borkobt@gmail.com>
2024-03-03 23:26:29 +0100

    pushing

--- bytebase/test/demo##202316410000##ddl##create_t3.sql
+++ bytebase/test/demo##202316410000##ddl##create_t3.sql
@@ -1,9 +1,9 @@
 CREATE TABLE
-   "public"."t2" (
+   "public"."t3" (
    "id" integer NOT NULL,
    "name" character varying(255) NOT NULL,
    PRIMARY KEY ("id")
 );
 
 COMMENT
-   ON COLUMN "public"."t2"."id" IS 'ID';
+   ON COLUMN "public"."t3"."id" IS 'ID';
